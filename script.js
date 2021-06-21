c98ed39bda670b49dc1e64cadff96271f06abf42
tomineodegard-kea <tomi0105@stud.kea.dk.com>
2021-06-21 13:03:23 +0200

    neu_card around pilot + slideshow from w3school

--- script.js
+++ script.js
@@ -18,3 +18,21 @@ function toggleMenu() {
     document.querySelector("#menuknap").textContent = "✕";
   }
 }
+
+// hele koden til slideshow tatt fra w3school
+var slideIndex = 0;
+carousel();
+
+function carousel() {
+  var i;
+  var x = document.getElementsByClassName("slideshowTomine");
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";
+  }
+  slideIndex++;
+  if (slideIndex > x.length) {
+    slideIndex = 1;
+  }
+  x[slideIndex - 1].style.display = "block";
+  setTimeout(carousel, 2000); // Change image every 2 seconds
+}
